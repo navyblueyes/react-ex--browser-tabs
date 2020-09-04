12b4384cba55b7b8494c91f1e4d1061bab2ef4ff
navyblueyes <lock_ed_on@yahoo.com>
2020-09-04 16:03:24 -0700

    Add React-Router; Tabs are new pages

--- src/App.js
+++ src/App.js
@@ -1,50 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Routes from './components/Routes';
 
-function Tab({children}) {
-  const [highlightStyle, setHighlightStyle] = useState({ left: 0, opacity: 0 });
-
-  function moveHighlight(e) {
-    setHighlightStyle({
-      left: e.nativeEvent.layerX - 150,
-    });
-  }
-
-  function hideHighlight(e) {
-    setHighlightStyle({
-      opacity: 0
-    });
-  }
-  
-  return (
-    <div className="tab" onMouseOut={hideHighlight} onMouseMove={moveHighlight}>
-    <div className="highlight" style={highlightStyle} />
-      {children}
-    </div>
-  );
-}
 
 function App() {
-
-
   return (
-    <div className="app">
-      <div className="browser">
-        <div className="tabs">
-          <Tab> 
-            <a>Home</a>
-          </Tab>
-          <Tab> 
-            <a>About</a> 
-          </Tab>
-          <Tab> 
-            <a>Features</a> 
-          </Tab>
+    <Router>
+      <div className="app">
+        <div className="browser">
+          <Header />
+          <div className="viewport">
+            <Routes />
+          </div>
         </div>
-
-        <div className="viewport">Pages Go Here</div>
       </div>
-    </div>
+    </Router>
   );
 }
 
