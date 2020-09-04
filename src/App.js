4532a0dc006bb6888c8f3c359179694f81c49a95
navyblueyes <lock_ed_on@yahoo.com>
2020-09-04 13:26:09 -0700

    started tabs; tabs animated with mouse movement

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function Tab({children}) {
+  const [highlightStyle, setHighlightStyle] = useState({ left: 0, opacity: 0 });
+
+  function moveHighlight(e) {
+    setHighlightStyle({
+      left: e.nativeEvent.layerX - 150,
+    });
+  }
+
+  function hideHighlight(e) {
+    setHighlightStyle({
+      opacity: 0
+    });
+  }
+  
+  return (
+    <div className="tab" onMouseOut={hideHighlight} onMouseMove={moveHighlight}>
+    <div className="highlight" style={highlightStyle} />
+      {children}
+    </div>
+  );
+}
+
+function App() {
+
+
+  return (
+    <div className="app">
+      <div className="browser">
+        <div className="tabs">
+          <Tab> 
+            <a>Home</a>
+          </Tab>
+          <Tab> 
+            <a>About</a> 
+          </Tab>
+          <Tab> 
+            <a>Features</a> 
+          </Tab>
+        </div>
+
+        <div className="viewport">Pages Go Here</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
